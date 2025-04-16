b48704e9a5db2d98cd2d1991fe1b817f3e538383
jyoon66 <asian.tiger.juice@gmail.com>
2025-04-15 23:51:13 -0700

    Apr 15 2025 files

--- Section1_Simple_Storage/7_Functions/SimpleStorage.sol
+++ Section1_Simple_Storage/7_Functions/SimpleStorage.sol
@@ -8,5 +8,16 @@ contract SimpleStorage {
     function store(uint256 _favoriteNumber) public {
         // the variable favorite number is updated with the value that is contained into the parameter `_favoriteNumber`
         favoriteNumber = _favoriteNumber;
+        uint 256 testVar = 5;
+    }
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        uint256 testVar = 5;
+    }
+
+    function something() public {
+        testVar = 6; // will raise a compilation error
+        favoriteNumber = 7; // this can be accessed because it's in the main contract scope
     }
 }
