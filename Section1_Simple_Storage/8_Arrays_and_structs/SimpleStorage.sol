acea93ced4ed5affe5339d1b1fddabc84f6c8ac2
jyoon66 <asian.tiger.juice@gmail.com>
2025-04-23 20:05:21 -0700

    Apr 23 2025

---
+++ Section1_Simple_Storage/8_Arrays_and_structs/SimpleStorage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber; // storage variable: it's stored in a section of the blockchain called "Storage"
+
+    function store(uint256 _favoriteNumber) public {
+        // the variable favorite number is updated with the value that is contained into the parameter `_favoriteNumber`
+        favoriteNumber = _favoriteNumber;
+        uint 256 testVar = 5;
+    }
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        uint256 testVar = 5;
+    }
+
+    function something() public {
+        testVar = 6; // will raise a compilation error
+        favoriteNumber = 7; // this can be accessed because it's in the main contract scope
+    }
+}
